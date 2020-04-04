41a48a2f3e779c9824a3e51a4c1ada29b9a82676
Flinn <358530466@qq.com>
2020-04-05 00:56:34 +0800

    deploy

---
+++ precache-manifest.11e592a8261f31b8a0114ac6d5c404e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d099c9d5b0f62b5b8df9",
+    "url": "/smtally-website/css/app.92e476ef.css"
+  },
+  {
+    "revision": "ec06de2708ce7ff4495d51ce93ae8df0",
+    "url": "/smtally-website/index.html"
+  },
+  {
+    "revision": "d099c9d5b0f62b5b8df9",
+    "url": "/smtally-website/js/app.5bbc55df.js"
+  },
+  {
+    "revision": "85deb01f0cf09b2d077a",
+    "url": "/smtally-website/js/chunk-vendors.ebc5d73e.js"
+  },
+  {
+    "revision": "df5b8d0102f8b523c8bf7b999111f176",
+    "url": "/smtally-website/manifest.json"
+  },
+  {
+    "revision": "d93236d865fda89be2b7cb92bb089d41",
+    "url": "/smtally-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/smtally-website/robots.txt"
+  }
+]);
